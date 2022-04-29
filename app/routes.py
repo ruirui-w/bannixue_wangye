c00dca9dc1381c12a0022406923b5d93b7deda06
ruirui-w <1569428479@qq.com>
2022-04-30 00:58:31 +0800

    url_for edition

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template,flash,redirect
+from flask import render_template, flash, redirect, url_for
 
 from app import app  # 从app包中导入 app这个实例
 
@@ -24,10 +24,10 @@ def index():
 		}
 	]
 	return render_template('index.html', user=user,posts=posts)
-@app.route('/login',methods=['GET','POST'])#既支持注册，又支持登录
+@app.route('/loginxx',methods=['GET','POST'])#既支持注册，又支持登录
 def login():
 	form = LoginForm()
 	if form.validate_on_submit():
 		flash('Login requested for user {},remember_me={}'.format(form.username.data,form.remember_me.data))
-		return redirect('/index')
+		return redirect(url_for('index'))
 	return render_template('login.html',title='Sign In',form=form)

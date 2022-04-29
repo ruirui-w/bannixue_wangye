fb63a05a1c2cf5623e0914ebb50244c57cdc9f0d
ruirui-w <1569428479@qq.com>
2022-04-30 00:38:20 +0800

    table

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
-from flask import Flask#从flask包中导入Flask类
+from flask import Flask  # 从flask包中导入Flask类
 
-app = Flask(__name__)#将Flask类的实例 赋值给名为 app 的变量。这个实例成为app包的成员。
-
-print('等会谁（哪个包或模块）在使用我：',__name__)
-
-from app import routes#从app包中导入模块routes #先执行其他语句
+from config import Config#从init代表了app,config模块导入Config类
+app = Flask(__name__)  # 将Flask类的实例 赋值给名为 app 的变量。这个实例成为app包的成员。
+app.config.from_object(Config)
+print(app.config['SECRET_KEY'])
+from app import routes  # 从app包中导入模块routes #先执行其他语句
--- app/routes.py
+++ app/routes.py
@@ -1,23 +1,33 @@
-from flask import render_template
+from flask import render_template,flash,redirect
 
 from app import app  # 从app包中导入 app这个实例
 
 
 # 2个路由
+from app.form import LoginForm
+
+
 @app.route('/')
 @app.route('/index')
 # 1个视图函数
 def index():
-    user = {'username': 'Miguel'}
-    # 创建一个列表：帖子。里面元素是两个字典，每个字典里元素还是字典，分别作者、帖子内容。
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-        'author': {'username': 'Susan'},
-        'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html', user=user,posts=posts)
+	user = {'username': 'Miguel'}
+	# 创建一个列表：帖子。里面元素是两个字典，每个字典里元素还是字典，分别作者、帖子内容。
+	posts = [
+		{
+			'author': {'username': 'John'},
+			'body': 'Beautiful day in Portland!'
+		},
+		{
+		'author': {'username': 'Susan'},
+		'body': 'The Avengers movie was so cool!'
+		}
+	]
+	return render_template('index.html', user=user,posts=posts)
+@app.route('/login',methods=['GET','POST'])#既支持注册，又支持登录
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		flash('Login requested for user {},remember_me={}'.format(form.username.data,form.remember_me.data))
+		return redirect('/index')
+	return render_template('login.html',title='Sign In',form=form)

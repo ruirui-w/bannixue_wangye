bb38bd635c66a9c04b7ab6f2a7f5eda3d136d580
ruirui-w <1569428479@qq.com>
2022-04-30 16:52:35 +0800

    sql

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,13 @@
-from flask import Flask  # 从flask包中导入Flask类
+from flask import Flask
+from config import Config#从config模块导入Config类
 
-from config import Config#从init代表了app,config模块导入Config类
-app = Flask(__name__)  # 将Flask类的实例 赋值给名为 app 的变量。这个实例成为app包的成员。
+from flask_sqlalchemy import SQLAlchemy#从包中导入类
+from flask_migrate import Migrate
+
+app = Flask(__name__)
 app.config.from_object(Config)
-print(app.config['SECRET_KEY'])
-from app import routes  # 从app包中导入模块routes #先执行其他语句
+
+db = SQLAlchemy(app)#数据库对象
+migrate = Migrate(app, db)#迁移引擎对象
+
+from app import routes,models#导入一个新模块models，它将定义数据库的结构，目前为止尚未编写
